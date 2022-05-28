e58e1f06d529ff3bb18e3ca35a6c1d38b6efc326
gutmanndev <lucas.garcia01@unicatolica.edu.co>
2022-05-28 09:39:25 -0500

    previos release version

--- src/main.js
+++ src/main.js
@@ -1,45 +1,40 @@
 function convert(){
-    let input = mainInput.text 
-    let option = optionList.currentIndex
-    console.log(option)
+    //CONVERTION VALUES
+    var pxToVw = 0.11086474501108648;
+    var pxToVh = 0.052083333333333336;
+    var pxToEm = 0.063;
+    var VwToPx = 19.2;
+    var VhToPx = 9.02;
+    var EmToPx = 16;
+    var myInput = document.getElementById('myInput');
+    var myOutput = document.getElementById('myOutput');
+    var options = document.getElementById('options');
+    var option = options.options[options.selectedIndex].value;
 
-    //valores
-    let pxToVw = 0.11086474501108648
-    let pxToVh = 0.052083333333333336
-    let pxToEm = 0.063
-    let VwToPx = 19.2
-    let VhToPx = 9.02
-    let EmToPx = 16
-
-    if (option == 0) { 
-        res.text = parseFloat(input * pxToVw).toFixed(5)
-        inputUnit.text = ("Px")
-        outputUnit.text = ("Vw")
-        //pixel to viewport width
+    if (option == 0) {
+        console.log(option)
+        myOutput.value = parseFloat(myInput.value * pxToVw).toFixed(5)
+        //pixel to viewport Width
     } else if (option == 1) {
-        res.text = parseFloat(input * pxToVh).toFixed(5)
-        inputUnit.text = ("Px")
-        outputUnit.text = ("Vh")
+        console.log(option)
+        myOutput.value = parseFloat(myInput.value * pxToVh).toFixed(5)
         //pixel to viewport Height
     } else if (option == 2){
-        res.text = parseFloat(input * pxToEm).toFixed(5)
-        inputUnit.text = ("Px")
-        outputUnit.text = ("Em")
+        console.log(option)
+        myOutput.value = parseFloat(myInput.value * pxToEm).toFixed(5)
         //pixel to Em
     } else if (option == 3){
-        res.text = parseFloat(input * VwToPx).toFixed(5)
-        inputUnit.text = ("Vw")
-        outputUnit.text = ("Px")
+        console.log(option)
+        myOutput.value = parseFloat(myInput.value * VwToPx).toFixed(5)
         //viewport width to Pixel
-    } else if (option == 4){//pixel to viewport
-        res.text = parseFloat(input * VhToPx).toFixed(5)
-        inputUnit.text = ("Vh")
-        outputUnit.text = ("Px")
+    } else if (option == 4){
+        console.log(option)
+        myOutput.value = parseFloat(myInput.value * VhToPx).toFixed(5)
         //viewport Height to Pixel
-    } else if (option == 5){//pixel to viewport
-        res.text = parseFloat(input * EmToPx).toFixed(5)
-        inputUnit.text = ("Em")
-        outputUnit.text = ("Px")
+    } else if (option == 5){
+        console.log(option)
+        myOutput.value = parseFloat(myInput.value * EmToPx).toFixed(5)
         //Em to Pixel
     }
+    
 }
