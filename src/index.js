d85755ddd07030b898502ecfba05fb05a7d9215b
gutmanndev <lucasgutmann0@gmail.com>
2022-06-22 18:46:12 -0500

    before changes dist

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 640,
-    height: 600,
+    height: 350,
     transparent: true
   });
 
@@ -19,7 +19,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
